77f27ef586a50b3ea02ede51076f0b81b17288b4
Jher Tagacay <tagacayj@gmail.com>
2026-03-26 18:11:49 +0800

    Cloned and removed embedded repo of pw-practice app and modify reusableMethods and TC01

--- playwright/global/reusableMethods.ts
+++ playwright/global/reusableMethods.ts
@@ -13,4 +13,37 @@ export class reusableMethods {
         await locator.click({force: true});
         console.log(`✅  Clicked the ${locator.toString()}`);
     }
+
+    // ==============================
+    // 📌 Environment Helper
+    // ==============================
+
+    /**
+        @param env - select "test" if http://localhost:4200/
+    */
+    async goToEnvironment(env: string) {
+        const envUrls: Record<string, string> = {
+            test    :   'http://localhost:4200/',
+        }
+        const url = envUrls[env.toLowerCase()]
+        if(!url) {
+            throw new Error("Unknown Environment");
+        }
+        else {
+            // Add this object for ad block
+            await this.page.route('**/*', route => {
+            const reqUrl = route.request().url();
+            const adBlockList = [
+                    'googleads', 'g.doubleclick', 'googlesyndication',
+                    'adservice', 'analytics', 'facebook.com/tr'
+                ];
+                if(adBlockList.some(ad => reqUrl.includes(ad))) {
+                    return route.abort();
+                }
+                return route.continue();
+            });
+            await this.page.goto(url, {timeout: 60000, waitUntil: 'domcontentloaded'})
+            console.log(`✅ Able to access the url ${url.toString()}`)
+        }
+    }
 }
