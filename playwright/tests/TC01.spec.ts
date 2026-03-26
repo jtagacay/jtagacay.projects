4f45e93eff223203536e1118130542d84f9ba8bf
Jher Tagacay <tagacayj@gmail.com>
2026-03-26 18:11:49 +0800

    Cloned and removed embedded repo of pw-practice app and modify reusableMethods and TC01

--- playwright/tests/TC01.spec.ts
+++ playwright/tests/TC01.spec.ts
@@ -6,6 +6,13 @@ test.describe.serial(`TC01`, () => {
     let rm: reusableMethods
 
     test.beforeAll(async({ browser }) => {
-        
+        const context = await browser.newContext()
+        page = await browser.newPage()
+        rm = new reusableMethods(page)
+        await rm.goToEnvironment('test');
+    })
+    test('Test only', {tag: '@P1'}, async () => {
+        await page.waitForTimeout(10000)
+        console.log('Made it here!')
     })
 });
