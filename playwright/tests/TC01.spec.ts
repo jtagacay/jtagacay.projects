9906f84ac3bd73697e883455fb1215ce01a0cadd
Jerism <jher.tester@gmail.com>
2026-03-27 22:05:03 +0800

    Modify the TC01 test

--- playwright/tests/TC01.spec.ts
+++ playwright/tests/TC01.spec.ts
@@ -1,18 +1,27 @@
 import { test, Page } from "playwright/test";
 import { reusableMethods } from "../global/reusableMethods";
+import { iotDashboard } from "../steps/iotDashboard";
 
 test.describe.serial(`TC01`, () => {
     let page: Page
     let rm: reusableMethods
+    let iot: iotDashboard
 
     test.beforeAll(async({ browser }) => {
         const context = await browser.newContext()
         page = await browser.newPage()
         rm = new reusableMethods(page)
+        iot = new iotDashboard(page)
         await rm.goToEnvironment('test');
     })
-    test('Test only', {tag: '@P1'}, async () => {
-        await page.waitForTimeout(10000)
-        console.log('Made it here!')
+    test('Verify the menu inside the top bar container', {tag: '@P1'}, async () => {
+        await iot.verifySidebarToggleIsVisble();
+        await iot.verifyTextLogoIsVisible()
+        await iot.verifyAppearanceToggleIsVisible()
+        await iot.verifySearchIsVisible()
+        await iot.verifyEmailIsVisible()
+        await iot.verifyNotificationIsVisible()
+        await iot.verifyProfileAvatarIsVisible()
+        await iot.verifyProfileNameIsVisible()
     })
 });
